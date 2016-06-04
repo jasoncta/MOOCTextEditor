321a9a4a10ba1377b79e72f682724c8abfc80411
Jason Tan <jasoncta@usc.edu>
2016-06-04 13:17:38 -0700

    Implemented and tested DictionaryBST

--- src/spelling/DictionaryBST.java
+++ src/spelling/DictionaryBST.java
@@ -1,5 +1,6 @@
 package spelling;
 
+import java.util.Objects;
 import java.util.TreeSet;
 
 /**
@@ -9,6 +10,10 @@ import java.util.TreeSet;
 public class DictionaryBST implements Dictionary 
 {
    private TreeSet<String> dict;
+   
+   public DictionaryBST() {
+	   dict = new TreeSet<String>();
+   }
 	
     // TODO: Implement the dictionary interface using a TreeSet.  
  	// You'll need a constructor here
@@ -21,6 +26,12 @@ public class DictionaryBST implements Dictionary
      * (it wasn't already there). */
     public boolean addWord(String word) {
     	// TODO: Implement this method
+    	Objects.requireNonNull(word);
+    	String wordToLowerCase = word.toLowerCase();
+    	if (!dict.contains(wordToLowerCase)) {
+    		dict.add(wordToLowerCase);
+    		return true;
+    	}
         return false;
     }
 
@@ -29,13 +40,19 @@ public class DictionaryBST implements Dictionary
     public int size()
     {
     	// TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
     	//TODO: Implement this method
-        return false;
+    	String sToLowerCase = s.toLowerCase();
+    	if (dict.contains(sToLowerCase)) {
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
     }
 
 }
